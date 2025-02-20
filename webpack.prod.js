71282da37edcd10ac9a23976dc5a746eedd5851f
AaronBarcos <abarcoscaballero@gmail.com>
2025-02-20 10:15:23 +0100

    Add new assets and update production configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
+        path: path.resolve(__dirname, 'build'),
         publicPath: '/'
     },
     plugins: [
